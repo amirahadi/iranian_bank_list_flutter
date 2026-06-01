1409a32d10cb75abfd716840a9a41e26784fb52d
Amir Ahadi <49079889+amirahadi@users.noreply.github.com>
2026-06-02 02:36:16 +0330

    comments changed
    library name removed, example version changed

--- lib/iranian_banks.dart
+++ lib/iranian_banks.dart
@@ -1,11 +1,10 @@
-/// A powerful and comprehensive library for working with Iranian bank data.
-///
-/// This library provides functionalities to:
-/// - Get bank information from a card number or IBAN.
-/// - Validate card numbers and IBANs using standard algorithms.
-/// - Access a list of all supported banks.
-/// - Display bank logos easily using the provided `BankInfoView` model.
-library iranian_banks;
+// A powerful and comprehensive library for working with Iranian bank data.
+//
+// This library provides functionalities to:
+// - Get bank information from a card number or IBAN.
+// - Validate card numbers and IBANs using standard algorithms.
+// - Access a list of all supported banks.
+// - Display bank logos easily using the provided `BankInfoView` model.
 
 export 'src/bank_service.dart';
 export 'src/model/bank_view_model.dart';
